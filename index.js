89e24a6d8cca24f65be0a32e0d03ad36a2044680
Mateu Aguiló Bosch <mateu.aguilo.bosch@gmail.com>
2017-10-29 23:32:07 +0100

    fix(Types): Expose the plugin type loader

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
-// Uncomment this to debug step-by-step through the source scripts, not the
-// compiled ones.
-// require('flow-remove-types/register');
-// const PluginManager = require('./src/PluginManager');
-// const PluginBase = require('./src/PluginBase');
-
 const PluginManager = require('./lib/PluginManager');
 const PluginLoaderBase = require('./lib/PluginLoaderBase');
+const PluginTypeLoaderBase = require('./lib/PluginTypeLoaderBase');
 const PluginLoaderFactory = require('./lib/PluginLoaderFactory');
 
 module.exports = {
   PluginManager,
   PluginLoaderBase,
+  PluginTypeLoaderBase,
   PluginLoaderFactory,
 };
